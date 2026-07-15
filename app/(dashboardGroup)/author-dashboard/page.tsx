e409ca84698af4555234a4e0a68d069496bac487
Hayder Ali <hayderbd4290@gmail.com>
2026-07-16 00:04:33 +0600

    initial setup done

---
+++ app/(dashboardGroup)/author-dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthorDashBoardPage = () => {
+  return (
+    <div>AuthorDashBoardPage</div>
+  )
+}
+
+export default AuthorDashBoardPage
---
+++ app/(dashboardGroup)/dashboard/my-post/page.tsx
@@ -0,0 +1,8 @@
+
+const UserMyPostPage = () => {
+  return (
+    <div>UserMyPostPage</div>
+  )
+}
+
+export default UserMyPostPage
