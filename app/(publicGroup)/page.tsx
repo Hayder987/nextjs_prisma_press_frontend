b9d996f110e4eaf49d6cd87d982099ea4614e947
Hayder Ali <hayderbd4290@gmail.com>
2026-07-16 00:49:04 +0600

    create login form wirh shadcn

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
    <div className="">
     hello next js
+    <Button
+    className="bg-red-700"
+    size={"sm"}
+    >Click me</Button>
    </div>
   );
 }
